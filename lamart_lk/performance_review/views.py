5e24942f06c8a0de975dc9806efcc71fb9589fe1
balashov <sashabalashov2019@gmail.com>
2023-08-03 13:11:58 +0500

    views serializers variant 2

--- lamart_lk/performance_review/views.py
+++ lamart_lk/performance_review/views.py
@@ -1,14 +1,18 @@
 from .forms import *
 from rest_framework import generics
-
 from .models import Form
 from .serializers import FormSerializer
 
-class FormAPIList(generics.ListCreateAPIView):
+class Form_POST_READ(generics.ListCreateAPIView): # чтение всех форм и добавление
      queryset = Form.objects.all()
      serializer_class = FormSerializer
-class FormAPIDetail(generics.RetrieveUpdateDestroyAPIView):
+
+class Form_EDIT(generics.RetrieveUpdateDestroyAPIView): # изменение и удаление
      queryset = Form.objects.all()
      serializer_class = FormSerializer
+     #permission_classes = (IsAdminUser)
+
 def new_review_form(request):
     return 0 # отрендерить шаблон с формой
+
+
