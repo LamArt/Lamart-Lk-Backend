ea10d7930333cf803efb303523b3e918bd4761cb
simizzz <merander207203@gmail.com>
2023-11-20 17:27:30 +0500

    add new urls

--- lamart_lk/performance_review/urls.py
+++ lamart_lk/performance_review/urls.py
@@ -4,4 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('teammates/', TeammatesAPIView.as_view(), name='get teammates'),
+    path("employee_forms/", EmployeeFormAPIView.as_view(), name='create employee form'),
+    path('employee_forms/users/{str:username}/', UserEmployeeFormsAPIView.as_view(), name='get employee form by username'),
+    path('teamlead_forms/', TeamleadFeedbackFormAPIView.as_view(), name="create teamlead feedback form"),
+
 ]
