a86754de20c50568f5fdfd0bb43783aac692821b
Ivan kh <khokhlovii@yandex.ru>
2023-11-21 18:14:57 +0300

    ports 5001-5004 added to allowed origins

--- lamart_lk/lamart_lk/settings.py
+++ lamart_lk/lamart_lk/settings.py
@@ -63,7 +63,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5001",
     "http://localhost:5002",
+    "http://localhost:5003",
+    "http://localhost:5004",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
